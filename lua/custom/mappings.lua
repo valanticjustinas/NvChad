f6944d53e5bc6e9f152f86156d66742b626b6a82
Justinas Garipovas <justinas.garipovas@cec.valatic.com>
2023-12-07 17:32:11 +0200

    feat: added custom cheatsheet key

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,6 +4,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>nc"] = { "<cmd> NvCheatsheet <CR>", "Mapping cheatsheet" },
   },
   v = {
     [">"] = { ">gv", "indent"},
