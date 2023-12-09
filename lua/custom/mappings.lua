e454d89339753d33e5befa0ee774bab37ed2f6f0
Justinas Garipovas <justinas.garipovas@cec.valatic.com>
2023-12-09 17:17:14 +0200

    refactor: DAP

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,6 +4,11 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>nc"] = { "<cmd> NvCheatsheet <CR>", "Mapping cheatsheet" },
+    -- DAP
+    ["<leader>d0"] = { function () require("dap").step_out() end, desc = "Step out" },
+    ["<leader>do"] = { function () require("dap").step_over() end, desc = "Step over" },
+    ["<leader>da"] = { function () require("dap").continue() end, desc = "Run" },
+
   },
   v = {
     [">"] = { ">gv", "indent"},
